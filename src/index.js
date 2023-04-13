ec80352d509a8676f3a416b3447ca0e92b0ffaff
Santiago Salcedo <santiagosalcedo.personal@gmail.com>
2023-04-13 07:32:17 -0500

    Aprendi mas de componentes y como pasarle algo diferente por dentro para reutilizar

--- src/index.js
+++ src/index.js
@@ -2,11 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Cabeza, UserCard } from './Cabeza'
 import Product, { Navbar } from './Product'
+import { Button } from './boton'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
-root.render(<>
-    <UserCard 
+root.render(
+    <>
+
+    <Button text='Click me' />
+    <Button text='Pay' />
+    <Button text='hello world' name='joe'/>
+    {/* <UserCard 
         name='Ryan Ray' 
         amount={3000} 
         married={true} 
@@ -14,4 +20,15 @@ root.render(<>
         address={{ street: '123 Main Street', city: 'New York' }} 
         greet={function () { alert('Hello')}}
     />
+
+    <UserCard
+        name='Joe Mcmillan'
+        amount={1000}
+        married={false}
+        points={[100,20]}
+        address={{
+            street: 'av some 123',
+            city: 'new york'
+        }}
+    /> */}
 </>)
