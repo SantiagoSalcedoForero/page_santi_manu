8577fbb79120034796de58c07a2618334c9a8bf7
Santiago Salcedo <santiagosalcedo.personal@gmail.com>
2023-04-20 11:35:34 -0500

    arreglando head

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,14 +4,23 @@ import '../styles/Header/style.css'
 import '@fortawesome/fontawesome-free/css/all.min.css';
 
 export function Header() {
+    
+    const [isOpen, setIsOpen] = useState(false)
+    const handleIconClick = () => {
+        setIsOpen(!isOpen)
+    }
+    const handleCloseClick = () => {
+        setIsOpen(false)
+    }
     return (
         <div className='header'>
             <img className='logoHead' src={process.env.PUBLIC_URL + '/assets/img/logo_santi_manu.png'} alt='logo Santi y Manu' />
-            <div className='headDerecho'>
+            <div className={`headDerecho ${isOpen && 'open'}`}>
                 <Link className='linkHeader' to='../pages/Nosotros'>Nosotros</Link>
                 <Link className='linkHeader' to='../pages/Podcast'>Podcast</Link>
             </div>
-            <i className='fa fa-bars' id='iconoHamburguesa' />
+            <i className='fa fa-bars' id={`iconoHamburguesa ${isOpen && 'open'}`} onClick={ handleIconClick } style={{ display: isOpen ? 'none' : 'flex' }}/>
+            <i className='fas fa-times iconoX' id='iconoX' onClick={handleCloseClick} style={{ display: isOpen ? 'flex' : 'none' }}/>
         </div>
     )
 }
